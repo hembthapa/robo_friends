bd1dd8218c59afd11625f070af95094cace88155
HEM B THAPA <hem.b.thapa07@gmail.com>
2020-04-13 22:28:50 +1000

    Updates

---
+++ precache-manifest.5180a83f195794f45853762d5fe71d62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0ca90d731608984cc06deef98f1151",
+    "url": "/robo_friends/index.html"
+  },
+  {
+    "revision": "3c68f85f661fb1e479a9",
+    "url": "/robo_friends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "1afb36dba5594bf08a07",
+    "url": "/robo_friends/static/css/main.87d549a4.chunk.css"
+  },
+  {
+    "revision": "3c68f85f661fb1e479a9",
+    "url": "/robo_friends/static/js/2.50887d7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo_friends/static/js/2.50887d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1afb36dba5594bf08a07",
+    "url": "/robo_friends/static/js/main.e498ce22.chunk.js"
+  },
+  {
+    "revision": "4536417f744e438ded41",
+    "url": "/robo_friends/static/js/runtime-main.67ade7c7.js"
+  },
+  {
+    "revision": "3f5c9111609279eb69be9e1a8a10e82b",
+    "url": "/robo_friends/static/media/SEGA-LOGO-FONT.ttf.3f5c9111.woff"
+  }
+]);
